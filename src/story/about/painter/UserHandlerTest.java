2abcc5c03e26d27bf75e053124a735ddb0dd6b39
ArthurkaLF <ibcserov@gmail.com>
2019-03-01 12:00:58 +0300

    Added a new method

--- src/story/about/painter/UserHandlerTest.java
+++ src/story/about/painter/UserHandlerTest.java
@@ -14,17 +14,17 @@ public class UserHandlerTest {
             Scanner in = new Scanner(System.in);
             System.out.print(text);
             String[] s = in.nextLine().replaceAll("[\\s]{2,}", " ").split(" ");
-            while (    (!s[0].equalsIgnoreCase("save"))
-                    && (!s[0].equalsIgnoreCase("sort"))
-                    && (!s[0].equalsIgnoreCase("info"))
-                    && (!s[0].equalsIgnoreCase("add"))
-                    && (!s[0].equalsIgnoreCase("import"))
-                    && (!s[0].equalsIgnoreCase("help"))
-                    && (!s[0].equalsIgnoreCase("show"))
-                    && (!s[0].equalsIgnoreCase("exit"))
-                    && (!s[0].equalsIgnoreCase("remove"))
-                    && (!s[0].equalsIgnoreCase("remove_lower"))
-                    && (!s[0].equalsIgnoreCase("start"))){
+            while (    (!s[0].equalsIgnoreCase("save"))         //
+                    && (!s[0].equalsIgnoreCase("sort"))         // +
+                    && (!s[0].equalsIgnoreCase("info"))         // +
+                    && (!s[0].equalsIgnoreCase("add"))          // +
+                    && (!s[0].equalsIgnoreCase("import"))       //
+                    && (!s[0].equalsIgnoreCase("help"))         //
+                    && (!s[0].equalsIgnoreCase("show"))         // +
+                    && (!s[0].equalsIgnoreCase("exit"))         // +
+                    && (!s[0].equalsIgnoreCase("remove"))       // +
+                    && (!s[0].equalsIgnoreCase("remove_lower")) //
+                    && (!s[0].equalsIgnoreCase("start"))){      //
                 System.out.println("Ошибка! Такой команды нет");
                 System.out.print("Введите команду управления коллекцией: ");
                 s = in.nextLine().replaceAll("[\\s]{2,}", " ").split(" ");
@@ -45,13 +45,24 @@ public class UserHandlerTest {
             switch (s[0]){
 
                 case "import":
+                    Command importFile;
+                    try{
+                        importFile = new ImportCommand(girlsHashSet,s[1]);
+                    }catch (ArrayIndexOutOfBoundsException e1){
+                        importFile = new ImportCommand(girlsHashSet, "test.csv");
+                    }
+                    navigator.setCommand(importFile);
+                    break;
 
+                case "help":
+                    System.out.println(help);
+                    break;
 
                 case "remove_lower": // ready
                     Command remove_lower;
                     try{
                         remove_lower = new RemoveLowerCommand(girlsHashSet,s[1]);
-                    }catch (ArrayIndexOutOfBoundsException ignored){
+                    }catch (ArrayIndexOutOfBoundsException e){
                         remove_lower = new RemoveLowerCommand(girlsHashSet,"");
                     }
                     navigator.setCommand(remove_lower);
@@ -109,51 +120,7 @@ public class UserHandlerTest {
 
 
 
-    //        if(s[0].equals("save")){
-    //            girlsHashSet.save();
-    //        }
-    //        else if(s[0].equals("sort")){
-    //            System.out.println(girlsHashSet.sortedList());
-    //        }
-    //        else if(s[0].equals("info")){
-    //            System.out.println(girlsHashSet.info());
-    //        }
-    //        else if(s[0].equals("remove_lower")){
-    //            try{
-    //                girlsHashSet.remove_lower(s[1]);
-    //            }catch (ArrayIndexOutOfBoundsException e1){
-    //                girlsHashSet.remove("");
-    //            }
-    //        }
-    //        else if(s[0].equals("show")){
-    //            System.out.println(girlsHashSet.show());
-    //        }
-    //        else if(s[0].equals("help")){
-    //            System.out.println(help);
-    //        }
-    //        else if(s[0].equals("import")){
-    //            try{
-    //
-    //
-    //                girlsHashSet.import_file(s[1]);
-    //            }catch (ArrayIndexOutOfBoundsException e1){
-    //                girlsHashSet.import_file("test.csv");
-    //            }
-    //        }
-    //        else if(s[0].equals("remove")){
-    //            girlsHashSet.remove(s[1]);
-    //        }
-    //        else if(s[0].equals("start")){
-    //            System.out.println("----------------------------------------\n");
-    //            UserHandler.startMainProgram(girlsHashSet);
-    //            System.out.println("----------------------------------------\n");
-    //        }
-    //        else if(s[0].equals("exit")){
-    //            System.out.println("Конец сеанса!!!");
-    //            System.out.println("Данные сохранены в Out.json");
-    //            return false;
-    //        }
-    //        return true;
+
     }
 
 }
---
+++ src/story/about/painter/commands/ImportCommand.java
@@ -0,0 +1,21 @@
+package story.about.painter.commands;
+
+import story.about.painter.Command;
+import story.about.painter.GirlsHashSet;
+
+public class ImportCommand implements Command {
+
+    private GirlsHashSet girlsHashSet;
+
+    public ImportCommand(GirlsHashSet girlsHashSet, String fileName) {
+        this.girlsHashSet = girlsHashSet;
+        this.fileName = fileName;
+    }
+
+    private String fileName;
+
+    @Override
+    public void execute() {
+        girlsHashSet.import_file(fileName);
+    }
+}
